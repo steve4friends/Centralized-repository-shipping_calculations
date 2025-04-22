7d9f5b9652a13d952f9443d2eefc86a855368d77
steve4friends <steve4friends16@gmail.com>
2025-04-22 05:57:11 -0400

    Revert "Aggiunta nuova riga"
    This reverts commit a51d2a0b881c23cc23727955c93c11db4187a2f7.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,4 +10,4 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
  # Ecco un nuovo aggiornamento da steve4friends
-# Here is another update by steve4friends
+
